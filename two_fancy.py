a04f16e23d6f47d297269b69ad1a6e56018de3f3
Mary E Gist <mgist56@ccs.local>
2019-10-17 11:16:38 -0400

    add two_fancy.py

--- two_fancy.py
+++ two_fancy.py
@@ -1,8 +1,7 @@
-import board
 from fancyLED import FancyLED
 
-fancy1 = FancyLED(board.D2, board.D3, board.D4)
-fancy2 = FancyLED(board.D5, board.D6, board.D7)
+fancy1 = FancyLED(2,3,4)
+fancy2 = FancyLED(5,6,7)
 
 while True:
     fancy1.alternate()
