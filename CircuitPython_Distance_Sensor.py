33afea610fe3c8ca7f53e81d23ed1bff6aae4fe9
Meg Gist <55596539+mgist56@users.noreply.github.com>
2019-11-13 13:54:33 -0500

    Update CircuitPython_Distance_Sensor.py

--- CircuitPython_Distance_Sensor.py
+++ CircuitPython_Distance_Sensor.py
@@ -6,7 +6,9 @@ import adafruit_hcsr04
 import neopixel
 import simpleio
 radishBat = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
+# "radishBat" being the distance sensor
 candyCorn = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=.1)
+# "candyCorn" being the NeoPixel
 r = 0
 b = 0
 g = 0
@@ -16,8 +18,10 @@ while True:
 # So turns out it's literally a function
     try:
         dist = radishBat.distance
+        # The color of the NeoPixel changes depending on the distance of the sensor
         if dist <= 20:
-            # map_range(x, domain min, domain max, range min, range max)
+            # Syntax (using domain and range in respect to treating like a function):
+            # map_range(x(in this case, distance), domain min, domain max, range min, range max)
             r = simpleio.map_range(dist, 0, 20, 255, 0)
             b = simpleio.map_range(dist, 5, 20, 0, 255)
             g = simpleio.map_range(dist, 20, 35, 0, 255)
@@ -27,5 +31,6 @@ while True:
             g = simpleio.map_range(dist, 20, 35, 0, 255)
         candyCorn.fill((int(r), int(g), int(b)))
     except RuntimeError:
+        # If there's an error print "boop" instead of attempting to change color
         print("boop")
         time.sleep(0.1)
